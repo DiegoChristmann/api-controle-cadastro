db76aaa274058ad0e450dd405138c23e021d1493
DiegoChristmann <dgchristmann28@gmail.com>
2026-03-11 13:37:50 -0300

    Alterei o binding dos dados

--- model/user.go
+++ model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	ID    int    `json:"id_user" db:"id" binding:"required"`
+	ID    int    `json:"id_user" db:"id"`
 	Name  string `json:"name" db:"user_name" binding:"required"`
 	Email string `json:"email" db:"email" binding:"required"`
 }
